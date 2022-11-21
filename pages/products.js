674f17e539995b36a8e3e2f724e205d39e3fdc40
rasvanjaya21 <mrsejati17@gmail.com>
2022-11-21 17:26:41 +0700

    update page same w/ each other

--- pages/products.js
+++ pages/products.js
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
-import { Fragment } from "react";
-import { Popover, Transition } from "@headlessui/react";
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import DeliveryAnimation from "../components/delivery/delivery-animation";
 import Link from "next/link";
 import Logo from "../components/logo/logo";
@@ -42,25 +40,33 @@ const navigation = [
 export default function Products() {
 
 
-	const [scrollPosition, setScrollPosition] = React.useState(0);
-	const [isScrolled, setIsScrolled] = React.useState(false);
+	const [scrollPosition, setScrollPosition] = useState(0);
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	const [isNavOpen, setIsNavOpen] = useState(false);
 
 	const handleScroll = () => {
 		const position = window.pageYOffset;
 		setScrollPosition(position);
 	};
 
-	React.useEffect(() => {
+	useEffect(() => {
+		if (isNavOpen) {
+			document.body.classList.add("overflow-hidden");
+		} else {
+			document.body.classList.remove("overflow-hidden");
+		}
+
 		window.addEventListener("scroll", handleScroll, { passive: true });
 
-		if (scrollPosition > 150) {
+		if (scrollPosition > 5) {
 			setIsScrolled(true);
 		} else {
 			setIsScrolled(false);
 		}
 
 		// getCity();
-	}, [scrollPosition, isScrolled]);
+	}, [scrollPosition, isScrolled, isNavOpen]);
 
 	return (
 		<>
@@ -72,115 +78,136 @@ export default function Products() {
 				/>
 				<link rel="icon" href="/logo.svg" />
 			</Head>
-			<div className="">
-				<div className="mx-auto">
-					<div className="p-5 mb-16 relative z-10 lg:p-24 ">
-						<Popover>
-							<div
-								className={
-									isScrolled
-										? `fixed right-0 top-0 z-20 bg-white/75 backdrop-blur-[15px] p-5 pt-5 lg:p-7 w-full sm:px-14 md:px-24 lg:px-24 lg:pt-6 shadow-primary transition-all`
-										: `fixed right-0 top-0 z-20 bg-white p-5 pt-12 lg:p-7 lg:pt-12 w-full sm:px-14 md:px-24 lg:px-24 `
-								}
+			<div className="overflow-hidden">
+				{isNavOpen ? (
+					<div
+						className={
+							isScrolled
+								? `md:hidden fixed p-5 pr-3 sm:pr-12 pt-5 lg:p-7 lg:pt-12 sm:px-14 md:px-24 lg:px-24 z-30 h-screen w-full bg-white/40 backdrop-blur-[10px]`
+								: `md:hidden fixed p-5 pr-3 sm:pr-12 pt-10 lg:p-7 lg:pt-12 sm:px-14 md:px-24 lg:px-24 z-30 h-screen w-full bg-white/40 backdrop-blur-[10px]`
+						}
+					>
+						<div
+							className="flex w-full items-center justify-between"
+							data-aos="fade-down"
+						>
+							<div>
+								<Logo />
+							</div>
+							<button
+								className="inline-flex justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
+								onClick={(event) => {
+									event.preventDefault();
+									setIsNavOpen(false);
+								}}
 							>
-								<nav
-									className="relative flex items-center justify-between"
-									aria-label="Global"
+								<XMarkIcon className="h-6 w-6" aria-hidden="true" />
+							</button>
+						</div>
+						<nav className="pt-12" data-aos="fade-down">
+							{navigation.map((item) => (
+								<Link
+									key={item.name}
+									href={item.href}
+									
+									onClick={item.onClick}
 								>
-									<div className="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
-										<div className="flex w-full items-center justify-between">
-											<Link
-												href="/"
-											>
-												<div className="flex">
-													<Logo />
-												</div>
-											</Link>
-											<div className="-mr-2 flex items-center md:hidden">
-												<Popover.Button className="inline-flex items-center justify-center rounded-md  p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500">
-													<span className="sr-only">Open main menu</span>
-													<Bars3Icon className="h-6 w-6" aria-hidden="true" />
-												</Popover.Button>
-											</div>
-										</div>
+									<div className={`font-medium ${item.class}`}>
+										{item.name}
 									</div>
-									<div className="hidden md:ml-10 md:block md:space-x-8">
-										{navigation.map((item) => (
-											<Link
-												key={item.name}
-												href={item.href}
-												className={`font-medium ${item.class}`}
-												onClick={item.onClick}
-											>
-												{item.name}
-											</Link>
-										))}
-									</div>
-								</nav>
-							</div>
-
-							<Transition
-								as={Fragment}
-								enter="duration-150 ease-out"
-								enterFrom="opacity-0 scale-95"
-								enterTo="opacity-100 scale-100"
-								leave="duration-100 ease-in"
-								leaveFrom="opacity-100 scale-100"
-								leaveTo="opacity-0 scale-95"
-							>
-								<Popover.Panel
-									focus
-									className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
-								></Popover.Panel>
-							</Transition>
-						</Popover>
-
-						<main className="mx-auto">
-							<div className=" bg-white">
-								<div
-									className="flex justify-center items-center"
-									data-aos="fade-left"
-								>
-									<div className="pointer-events-none">
-										<DeliveryAnimation />
-									</div>
-								</div>
-								<h1 className="m-10 flex text-xl font-bold text-gray-800 sm:place-content-center">
-									<span className="text-primary-600 mr-1">
-										Being Maintenanced :{" "}
-									</span>{" "}
-									Products Page is delivering to you
-									{/* //// */}
-								</h1>
-							</div>
-
-							<div className="absolute lg:fixed bottom-0 right-0 left-0 w-screen p-10 px-24">
-								<footer className="rounded-lg shadow  flex items-center justify-between p-6">
-									<span className="text-sm text-gray-500 text-center hover:text-primary-600">
-										© 2022 <a href="https://bradeer.my.id/">Bradeer.my.id™</a>
-									</span>
-									<ul className="flex items-center text-sm text-center text-gray-500 sm:mt-0 ml-10">
-										<li>
-											<a href="#" className="mr-4 hover:underline md:mr-6">
-												Privacy Policy
-											</a>
-										</li>
-										<li>
-											<a href="#" className="mr-4 hover:underline md:mr-6">
-												Licensing
-											</a>
-										</li>
-										<li>
-											<a href="#" className="hover:underline">
-												Contact
-											</a>
-										</li>
-									</ul>
-								</footer>
-							</div>
-						</main>
+								</Link>
+							))}
+						</nav>
 					</div>
+				) : (
+					<></>
+				)}
+				<div
+					className={
+						isScrolled
+							? `fixed right-0 top-0 z-20 bg-white/40 backdrop-blur-[15px] p-5 pt-5 lg:p-7 w-full sm:px-14 md:px-24 lg:px-24 lg:pt-6 shadow-primary transition-all duration-300`
+							: `fixed right-0 top-0 z-20 bg-white p-5 pt-10 lg:p-7 lg:pt-12 w-full sm:px-14 md:px-24 lg:px-24 transition-all duration-300 pb-5`
+					}
+				>
+					<nav
+						className="relative flex items-center justify-between sm:h-10"
+						aria-label="Global"
+					>
+						<div className="flex flex-shrink-0 flex-grow items-center lg:flex-grow-0">
+							<div className="flex w-full items-center justify-between">
+								<Link href="/">
+									<Logo />
+								</Link>
+								<div className="-mr-2 flex items-center md:hidden">
+									<button
+										className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
+										onClick={(event) => {
+											console.log("clicked");
+											setIsNavOpen(true);
+										}}
+									>
+										<Bars3Icon className="h-6 w-6" aria-hidden="true" />
+									</button>
+								</div>
+							</div>
+						</div>
+						<div className="hidden md:ml-10 md:block md:space-x-8">
+							{navigation.map((item) => (
+								<Link
+									key={item.name}
+									href={item.href}
+									className={`font-medium ${item.class}`}
+									onClick={item.onClick}
+								>
+									{item.name}
+								</Link>
+							))}
+						</div>
+					</nav>
 				</div>
+				<main className="m-auto">
+					<div className="pt-40 bg-white">
+						<div
+							className="pr-14 flex justify-center items-center"
+							data-aos="fade-in"
+						>
+							<div className="pointer-events-none">
+								<DeliveryAnimation />
+							</div>
+						</div>
+						<h1 className="m-2 sm:m-10 text-center text-xs sm:text-xl font-bold text-gray-800">
+							<span className="text-primary-600 mr-1">
+								Being Maintenanced :{" "}
+							</span>{" "}
+							Products Page is delivering to you
+							{/* //// */}
+						</h1>
+					</div>
+					<div className="fixed lg:fixed bottom-0 right-0 left-0 w-screen p-6 sm:px-14 md:px-24 lg:p-10 lg:px-24 lg:pt-0 pt-0 ">
+						<footer className="rounded-lg shadow  flex items-center justify-between p-4 lg:p-6 bg-white/40 backdrop-blur-[15px]">
+							<span className="inline text-xs lg:text-sm text-gray-500 text-center hover:text-primary-600">
+								© 2022 <a href="#">Bayarno™</a>
+							</span>
+							<ul className="flex items-center text-xs lg:text-sm text-center text-gray-500 sm:mt-0 lg:ml-10">
+								<li>
+									<a href="#" className="mr-4 hover:underline md:mr-6">
+										Privacy Policy
+									</a>
+								</li>
+								<li>
+									<a href="#" className="mr-4 hover:underline md:mr-6">
+										Licensing
+									</a>
+								</li>
+								<li>
+									<a href="#" className="hover:underline">
+										Contact
+									</a>
+								</li>
+							</ul>
+						</footer>
+					</div>
+				</main>
 			</div>
 		</>
 	);
